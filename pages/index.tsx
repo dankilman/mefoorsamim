c9f320128e3999f7404e80c232224c6077a69ea6
Dan Kilman <dan.kilman@forter.com>
2020-04-20 01:16:51 +0300

    create a simple index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,43 @@
+import {Button, Flex, Heading} from 'rebass'
+import {Input} from '@rebass/forms'
+import {Router, useRouter} from 'next/router'
+import {useState} from 'react'
+import Head from 'next/head'
+
 export default () => {
-  return <div />
+  const [roomName, setRoomName] = useState('')
+  const router = useRouter()
+
+  const goToRoom = () => roomName.trim() && router.push(`/play/${roomName.trim()}`)
+
+  return (
+    <Flex
+      flexWrap="wrap"
+      width={500}
+      justifyContent="center"
+      m="auto"
+    >
+      <Head>
+        <title>Mefoorsamim</title>
+      </Head>
+      <Heading width={1} mt={12} mb={1}>Mefoorsamim</Heading>
+      <Flex width={1} mt={2}>
+        <Input
+          flex={5}
+          mr={1}
+          placeholder="Enter Room Name"
+          value={roomName}
+          onInput={e => setRoomName((e.target as any).value)}
+          onChange={e => setRoomName((e.target as any).value)}
+          onKeyPress={e => e.key === 'Enter' && goToRoom()}
+        />
+        <Button
+          flex={1}
+          onClick={() => goToRoom() }
+        >
+          Play
+        </Button>
+      </Flex>
+    </Flex>
+  )
 }
--- pages/play/[gameID].tsx
+++ pages/play/[gameID].tsx
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router'
 import Lobby from '../../components/lobby'
+import {Box} from 'rebass'
+import Head from 'next/head'
 
 export default (props) => {
   const router = useRouter()
@@ -9,6 +11,11 @@ export default (props) => {
     return <div />
   }
   return (
-    <Lobby gameID={gameID as string} queryPlayerName={playerName as string}/>
+    <Box>
+      <Head>
+        <title>{gameID} | Mefoorsamim</title>
+      </Head>
+      <Lobby gameID={gameID as string} queryPlayerName={playerName as string}/>
+    </Box>
   )
 }
