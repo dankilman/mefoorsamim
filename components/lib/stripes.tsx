86ef3f1d051d86a661868e46b78710663855e74e
Dan Kilman <dan.kilman@forter.com>
2020-04-27 02:37:08 +0300

    make stripes pixes based on not percentage based

--- components/lib/stripes.tsx
+++ components/lib/stripes.tsx
@@ -3,6 +3,7 @@ import {useTheme} from 'emotion-theming'
 
 export default props => {
   const {colors: {c1, c2, c3, c4, c5}} = useTheme()
+  const increment = 300
   return (
     <Box
       {...props}
@@ -12,16 +13,16 @@ export default props => {
         background: `
           repeating-linear-gradient(
             45deg,
-            ${c1} 0%,
-            ${c1} 20%,
-            ${c2} 20%,
-            ${c2} 40%,
-            ${c3} 40%,
-            ${c3} 60%,
-            ${c4} 60%,
-            ${c4} 80%,
-            ${c5} 80%,
-            ${c5} 100%
+            ${c1} 0,
+            ${c1} ${increment}px,
+            ${c2} ${increment}px,
+            ${c2} ${increment * 2}px,
+            ${c3} ${increment * 2}px,
+            ${c3} ${increment * 3}px,
+            ${c4} ${increment * 3}px,
+            ${c4} ${increment * 4}px,
+            ${c5} ${increment * 4}px,
+            ${c5} ${increment * 5}px
           )
         `
       }}
